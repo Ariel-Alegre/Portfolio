d8ba4c72c76c43ba3a3d09416a23db1fc459abab
Ariel-Alegre <arielalegre98@gmail.com>
2023-01-20 09:28:38 -0300

    fix update

--- src/components/SkillSet/SkillSet.jsx
+++ src/components/SkillSet/SkillSet.jsx
@@ -12,14 +12,15 @@ const SkillSet = () => {
     <h2 className={styles.titleSkill}>S K I L L S E T</h2>
     <hr className={styles.linea} />
     <div  className={styles.skill}>       
-<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/html5/html5-original.svg' height='50px' width='50px'/>
-<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/css3/css3-original.svg' height='50px' width='50px'/>
-<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/javascript/javascript-original.svg' height='50px' width='50px'/>
-<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/react/react-original.svg' height='50px' width='50px'/>
-<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/redux/redux-original.svg' height='50px' width='50px'/>
-<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/nodejs/nodejs-original.svg' height='50px' width='50px'/>
-<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/postgresql/postgresql-original.svg' height='50px' width='50px'/>
-<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/sequelize/sequelize-original.svg' height='50px' width='50px'/>
+<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/html5/html5-original.svg' target='_blank'/>
+<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/css3/css3-original.svg'/>
+<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/bootstrap/bootstrap-original.svg'/>
+<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/javascript/javascript-original.svg' height='30px' width='30px'/>
+<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/react/react-original.svg' height='30px' width='30px'/>
+<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/redux/redux-original.svg' height='30px' width='30px'/>
+<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/nodejs/nodejs-original.svg' height='30px' width='30px'/>
+<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/postgresql/postgresql-original.svg' height='30px' width='30px'/>
+<img className={styles.img} src='https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/sequelize/sequelize-original.svg' height='30px' width='30px'/>
     </div>
     <hr className={styles.linea}/>
      <div>    
