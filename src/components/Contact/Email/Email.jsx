88ba24034c0f837ad45f16d17b889eea1438b31f
Ariel-Alegre <arielalegre98@gmail.com>
2023-02-09 10:53:06 -0300

    fix app update

--- src/components/Contact/Email/Email.jsx
+++ src/components/Contact/Email/Email.jsx
@@ -1,41 +1,46 @@
 import styles from "./Email.module.css";
 
 function MyEmail() {
+  
   return (
     <div className={styles.inputContainer}>
-
-    <form action="https://formsubmit.co/arielalegre98@gmail.com" method="POST">
-      <div class="mb-3">
-        <label for="exampleFormControlInput1" class="form-label">
-          Email address
-        </label>
-        <input
-          type="email"
-          class="form-control"
-          id="exampleFormControlInput1"
-          placeholder="name@example.com"
-          name="email"
+      <form
+        action="https://formsubmit.co/arielalegre98@gmail.com"
+        method="POST"
+      >
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name@example.com"
+            name="email"
           />
-      </div>
-      <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">
-          Comment
-        </label>
-        <textarea
-          class="form-control"
-          id="exampleFormControlTextarea1"
-          rows="3"
-          name="Comment"
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">
+            Comment
+          </label>
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+            name="Comment"
           ></textarea>
-        <input type="hidden" name="_next" value="https://repositorio-liart.vercel.app/home" />
-        <input type="hidden" name="_captcha" value="false" />
-      </div>
-      <button type="submit">Send</button>
-    </form>
-          </div>
+          <input
+            type="hidden"
+            name="_next"
+            value="https://repositorio-liart.vercel.app/home"
+          />
+          <input type="hidden" name="_captcha" value="false" />
+        </div>
+        <button  type="submit">Send</button>
+      </form>
+    </div>
   );
 }
-
-
 
 export default MyEmail;
